38bcc25366c7f25ab3ab66316566246d1f1eb971
Daniela Raygadas <draygadasdom@dxc.com>
2020-08-11 22:55:43 -0500

    Working Templates
    Templates for Wallet, buisness_delivery, business_review_order, and general_order.

--- fedex_economy_market.py
+++ fedex_economy_market.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 smart_contract = os.environ['SMART_CONTRACT']
-# smart_contract = 'https://fedex-economy-smartcontract.herokuapp.com/'
+#smart_contract = 'https://fedex-economy-smartcontract.herokuapp.com/'
 # smart_contract = 'http://127.0.0.1:5000/'
 
 @app.route('/')
